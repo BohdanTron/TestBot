32ef8f614a4808b279924a2f19f66c90b4f24bff
tronbodya <tronbodya@gmail.com>
2023-04-11 14:01:58 +0300

    app Id fix

--- EchoBot/Controllers/NotifyController.cs
+++ EchoBot/Controllers/NotifyController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace EchoBot.Controllers
@@ -15,16 +16,19 @@ namespace EchoBot.Controllers
     public class NotifyController : ControllerBase
     {
         private readonly IBotFrameworkHttpAdapter _adapter;
+        private readonly string _appId;
         private readonly ConcurrentDictionary<string, ConversationReference> _confConversationReferences;
 
         private readonly ILogger _logger;
 
         public NotifyController(
-            IBotFrameworkHttpAdapter adapter, 
+            IBotFrameworkHttpAdapter adapter,
+            IConfiguration configuration,
             ConcurrentDictionary<string, ConversationReference> confConversationReferences,
             ILogger<NotifyController> logger)
         {
             _adapter = adapter;
+            _appId = configuration["MicrosoftAppId"] ?? string.Empty;
             _confConversationReferences = confConversationReferences;
             _logger = logger;
         }
@@ -38,7 +42,7 @@ namespace EchoBot.Controllers
 
                 _logger.LogInformation(message);
 
-                await ((BotAdapter)_adapter).ContinueConversationAsync(string.Empty, conversation,
+                await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversation,
                     async (context, token) =>
                     {
                         await context.SendActivityAsync("Hi, it's a proactive message", cancellationToken: token);
